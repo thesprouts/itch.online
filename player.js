51e6adc547055f2bc8c5aac8bd8f196b375271bb
Alec Resnick <aresnickety@gmail.com>
2016-06-23 22:59:40 -0400

    Debugging CORS

--- player.js
+++ player.js
@@ -173,7 +173,8 @@ P.player = (function() {
 
   function load(id, cb, titleCallback) {
     P.player.projectId = id;
-    P.player.projectURL = id ? 'http://scratch.mit.edu/projects/' + id + '/' : '';
+    var corsProxy = 'http://crossorigin.me/';
+    P.player.projectURL = id ? corsProxy + 'http://scratch.mit.edu/projects/' + id + '/' : '';
 
     if (stage) {
       stage.stopAll();
